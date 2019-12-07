128de566e7cac22c0c6728d644d0892fa4c52025
gobuddy <shawn.lisheng@outlook.com>
2019-12-07 14:32:01 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=gobuddy
 
 export MYCLUSTER=tektonknativebeijing10
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AC/.bluemix/plugins/container-service/clusters/tektonknativebeijing10/kube-config-tok04-tektonknativebeijing10.yml
 
 export NAMESPACE=nemospace
 export EMAIL=shawn.lisheng@gmail.com
